a65f8532ee9dfa9ebcecaec66870ea0f5db8267e
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-06-06 10:44:35 +0200

    Latest updates to the volto PR supporting `public` folders per add-on

--- sub/project_settings/{{ cookiecutter.__folder_name }}/frontend/volto.config.js
+++ sub/project_settings/{{ cookiecutter.__folder_name }}/frontend/volto.config.js
@@ -1,9 +1,7 @@
 const addons = ['{{ cookiecutter.__npm_package_name }}'];
 const theme = '';
-const publicPath = '../../../public';
 
 module.exports = {
   addons,
-  theme,
-  publicPath
+  theme
 };
