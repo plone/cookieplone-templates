3f6c878f15a76849945d5772a96ab43543012af8
Nils Reuvers <nils.reuvers@sparkholland.com>
2024-12-08 14:12:30 +0000

    added BE bob files to MANIFEST and updated FE index.js

--- frontend_addon/{{ cookiecutter.__folder_name }}/packages/{{ cookiecutter.frontend_addon_name }}/src/index.js
+++ frontend_addon/{{ cookiecutter.__folder_name }}/packages/{{ cookiecutter.frontend_addon_name }}/src/index.js
@@ -1,4 +1,10 @@
 const applyConfig = (config) => {
+  config.settings = {
+    ...config.settings,
+    isMultilingual: false,
+    supportedLanguages: ['en'],
+    defaultLanguage: 'en',
+  };
   return config;
 };
 
