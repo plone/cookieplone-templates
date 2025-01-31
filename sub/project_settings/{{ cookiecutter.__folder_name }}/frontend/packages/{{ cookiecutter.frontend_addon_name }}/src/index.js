23bdc7304f954b103654c98498bbcea5abf97421
Thomas Mersch <mersch@kitconcept.com>
2025-01-31 10:13:29 +0100

    Fix lint
    Fix regarding https://github.com/plone/cookieplone-templates/issues/134

--- sub/project_settings/{{ cookiecutter.__folder_name }}/frontend/packages/{{ cookiecutter.frontend_addon_name }}/src/index.js
+++ sub/project_settings/{{ cookiecutter.__folder_name }}/frontend/packages/{{ cookiecutter.frontend_addon_name }}/src/index.js
@@ -1,7 +1,7 @@
 const applyConfig = (config) => {
   config.settings.isMultilingual = false;
-  config.settings.supportedLanguages = ["{{ cookiecutter.language_code }}"];
-  config.settings.defaultLanguage = "{{ cookiecutter.language_code }}";
+  config.settings.supportedLanguages = ['{{ cookiecutter.language_code }}'];
+  config.settings.defaultLanguage = '{{ cookiecutter.language_code }}';
 
   return config;
 };
