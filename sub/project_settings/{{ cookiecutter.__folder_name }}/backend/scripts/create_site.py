d816ca0131c4e6f21187487d8efcc4f35ab13bd4
Philip Bauer <bauer@starzel.de>
2025-02-12 15:40:50 +0100

    fix project

--- sub/project_settings/{{ cookiecutter.__folder_name }}/backend/scripts/create_site.py
+++ sub/project_settings/{{ cookiecutter.__folder_name }}/backend/scripts/create_site.py
@@ -27,7 +27,7 @@ def asbool(s):
 
 
 DELETE_EXISTING = asbool(os.getenv("DELETE_EXISTING"))
-{% if cookiecutter.plone_version >= "6.1" and cookiecutter.__feature_headless == "0" -%}
+{% if cookiecutter.__feature_headless == "1" -%}
 EXAMPLE_CONTENT = asbool(os.getenv("EXAMPLE_CONTENT", "1"))  # Create example content by default
 {%- endif %}
 
@@ -71,7 +71,7 @@ if site_id not in app.objectIds():
     portal_setup.runAllImportStepsFromProfile("profile-{{ cookiecutter.python_package_name }}:default")
     transaction.commit()
 
-{% if cookiecutter.__feature_headless == "1" -%}
+{% if cookiecutter.__feature_headless == "1" %}
     if EXAMPLE_CONTENT:
         portal_setup.runAllImportStepsFromProfile("profile-{{ cookiecutter.python_package_name }}:initial")
         transaction.commit()
