9f55341cf0083470af404e47410e8df644b423c8
Armin Stross-Radschinski <acsr.github@dev.acsr.de>
2025-03-26 15:57:04 +0100

    add pre_prompt.py hooks in main and docs template

--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -1,6 +1,7 @@
 """Pre Prompt hook."""
 
 import sys
+import os
 
 try:
     from cookieplone.utils import console
@@ -9,6 +10,8 @@ try:
 except ModuleNotFoundError:
     HAS_COOKIEPLONE = False
 
+# This fetches only the temporary folder
+pwd=os.path.dirname(os.getcwd())
 
 def main():
     """Check if we have cookieplone installed."""
@@ -18,6 +21,40 @@ def main():
     else:
         console.print_plone_banner()
 
+
+PLONE_LOGOTYPE_BANNER = """
+          *******                                                                        
+      ***************                                                                    
+    ***             ***        *********     ***                                     *** 
+  ***    ***          ***      ***********   ***                                    * R *
+ ***    *****          ***     ***      ***  ***                                     *** 
+***      ***            ***    ***       *** ***       ****     ***  ***       ****      
+***               ***   ***    ***      ***  ***     ********   *********    ********    
+***              *****  ***    ***********   ***    ***    ***  ****   ***  ***    ***   
+***      ***      ***   ***    *********     ***    ***    ***  ***    ***  **********   
+ ***    *****          ***     ***           ***    ***    ***  ***    ***  *********    
+  ***    ***          ***      ***           ****   ***    ***  ***    ***  ***    ...   
+    ***             ***        ***            *****  ********   ***    ***   ********    
+      ***************          ***              ***    ****     ***    ***     ****      
+          *******                                                                        
+"""
+
+def print_plone_logotype_banner():
+    """Print Plone Logotype banner."""
+    style: str = "bold"
+    color: str = "blue"
+    console.print(PLONE_LOGOTYPE_BANNER, style, color)
+
+def main():
+    """Check context."""
+    if not HAS_COOKIEPLONE:
+        print("This template should be run with cookieplone")
+        sys.exit(1)
+    else:
+        #console.print_plone_banner()
+        print_plone_logotype_banner()
+        console.print("the Cookieplone main tmp location:")
+        console.print(pwd)
 
 if __name__ == "__main__":
     main()
