7b543eafbdcf29a6af71ede34031053aedcb4367
Érico Andrei <ericof@gmail.com>
2025-03-31 13:13:22 -0300

    Reorganize templates structure (#187)
    * WIP: Organize repo
    
    * WIP: Reorganize templates
    
    * GHA: Fix functional tests
    
    * Add .env_gha to the devops folder of a project
    
    * Require cookieplone 0.9.4 or above
    
    * GHA: Add Report job to the end of the workflow
    
    * Apply suggestions from code review
    
    Co-authored-by: Steve Piercy <web@stevepiercy.com>
    
    ---------
    
    Co-authored-by: Steve Piercy <web@stevepiercy.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 
 import pytest
+from pytest_cookies.plugin import Cookies
 
 CONFIG_FILE = "cookiecutter.json"
 VALID_HOOK_NAMES = [
@@ -50,3 +51,92 @@ def templates(read_config, get_hooks) -> list:
             "hooks": get_hooks(path),
         }
     return templates
+
+
+@pytest.fixture(scope="session")
+def cookieplone_root() -> dict:
+    """Cookieplone root dir."""
+    folder = Path(__file__).parent
+    return folder.parent.resolve()
+
+
+@pytest.fixture(scope="module")
+def cookies_module(request, tmpdir_factory, _cookiecutter_config_file):
+    """Yield an instance of the Cookies helper class that can be used to
+    generate a project from a template.
+
+    Run cookiecutter:
+        result = cookies.bake(extra_context={
+            'variable1': 'value1',
+            'variable2': 'value2',
+        })
+    """
+    template_dir = request.config.option.template
+
+    output_dir = tmpdir_factory.mktemp(f"cookies-{request.module.__name__}")
+    output_factory = output_dir.mkdir
+
+    yield Cookies(template_dir, output_factory, _cookiecutter_config_file)
+
+    # Add option to keep generated output directories.
+    if not request.config.option.keep_baked_projects:
+        output_dir.remove()
+
+
+@pytest.fixture
+def volto_versions():
+    versions = ["17.15.5", "16.31.4", "18.0.0-alpha.27"]
+    return versions
+
+
+@pytest.fixture
+def plone_versions():
+    versions = [
+        "6.0.0",
+        "6.0.0a1",
+        "6.0.0a2",
+        "6.0.0a3",
+        "6.0.0a4",
+        "6.0.0a5",
+        "6.0.0a6",
+        "6.0.0b1",
+        "6.0.0b2",
+        "6.0.0b3",
+        "6.0.0rc1",
+        "6.0.0rc2",
+        "6.0.1",
+        "6.0.10",
+        "6.0.2",
+        "6.0.3",
+        "6.0.4",
+        "6.0.5",
+        "6.0.6",
+        "6.0.7",
+        "6.0.8",
+        "6.0.9",
+        "6.1.0a1",
+        "6.1.0a2",
+    ]
+    return versions
+
+
+@pytest.fixture(autouse=True)
+def mock_npm_packages(monkeypatch, volto_versions):
+    from cookieplone.utils import versions
+
+    def get_npm_package_versions(*args, **kwargs):
+        return volto_versions
+
+    monkeypatch.setattr(versions, "get_npm_package_versions", get_npm_package_versions)
+
+
+@pytest.fixture(autouse=True)
+def mock_pypi_packages(monkeypatch, plone_versions):
+    from cookieplone.utils import versions
+
+    def get_pypi_package_versions(*args, **kwargs):
+        return plone_versions
+
+    monkeypatch.setattr(
+        versions, "get_pypi_package_versions", get_pypi_package_versions
+    )
