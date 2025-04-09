a293e8ae0daae9a2d27c25e163145e3d9b845521
Steve Piercy <web@stevepiercy.com>
2025-04-09 02:08:58 -0700

    Add support for sphinx-tippy

--- templates/add-ons/documentation_starter/{{cookiecutter.__folder_name}}/docs/conf.py
+++ templates/add-ons/documentation_starter/{{cookiecutter.__folder_name}}/docs/conf.py
@@ -71,6 +71,7 @@ extensions = [
     "sphinx_examples",
     "sphinx_reredirects",
     "sphinx_sitemap",
+    "sphinx_tippy",
     "sphinxcontrib.httpdomain",  # plone.restapi
     "sphinxcontrib.httpexample",  # plone.restapi
     "sphinxcontrib.mermaid",
@@ -305,6 +306,16 @@ notfound_template = "404.html"
 # -- sphinx-reredirects configuration ----------------------------------
 # https://documatt.com/sphinx-reredirects/usage.html
 redirects = {}
+
+
+# -- sphinx-tippy configuration ----------------------------------
+tippy_anchor_parent_selector = "article.bd-article"
+tippy_enable_doitips = False
+tippy_enable_wikitips = False
+tippy_props = {
+    "interactive": True,
+    "placement": "auto-end",
+}
 
 
 # -- Options for HTML help output -------------------------------------------------
