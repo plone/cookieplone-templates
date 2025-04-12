08b3b05e9e84dc6ce10f8725349a319aae9f4f03
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-04-12 18:38:16 +0200

    Fix Cypress tests

--- seven_addon/{{ cookiecutter.__folder_name }}/cypress/tests/example.cy.js
+++ seven_addon/{{ cookiecutter.__folder_name }}/cypress/tests/example.cy.js
@@ -11,10 +11,8 @@ context('Example Acceptance Tests', () => {
       cy.autologin();
     });
 
-    it('As editor I can add edit a Page', function () {
+    it('As editor I can add visit a Page', function () {
       cy.visit('/document');
-      cy.navigate('/document/edit');
-      cy.get('#toolbar-save').click();
     });
   });
 });
