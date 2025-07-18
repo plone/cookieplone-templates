616b3f9275c69737b28bf20db048328041ac40cb
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-07-18 12:43:07 +0200

    Revert "fix tests"
    This reverts commit 20daf7c4cb866db1844193ad1630392f40b1931f.

--- tests/templates/add-ons/backend/conftest.py
+++ tests/templates/add-ons/backend/conftest.py
@@ -25,6 +25,8 @@ PKG_SRC_FILES = [
     "__init__.py",
     "configure.zcml",
     "content/__init__.py",
+    "controlpanel/__init__.py",
+    "controlpanel/configure.zcml",
     "dependencies.zcml",
     "indexers/__init__.py",
     "indexers/configure.zcml",
