af6f169e52cd63b00620ed1ef29ce97bd6abef5a
Érico Andrei <ericof@gmail.com>
2025-11-14 11:33:10 -0300

    Remove language setting from add-on

--- templates/sub/addon_settings/{{ cookiecutter.__folder_name }}/frontend/packages/{{ cookiecutter.frontend_addon_name }}/src/config/settings.ts
+++ templates/sub/addon_settings/{{ cookiecutter.__folder_name }}/frontend/packages/{{ cookiecutter.frontend_addon_name }}/src/config/settings.ts
@@ -1,10 +1,5 @@
 import type { ConfigType } from "@plone/registry";
 
 export default function install(config: ConfigType) {
-  // Language settings
-  config.settings.isMultilingual = false;
-  config.settings.supportedLanguages = ["{{ cookiecutter.language_code }}"];
-  config.settings.defaultLanguage = "{{ cookiecutter.language_code }}";
-
   return config;
 }
