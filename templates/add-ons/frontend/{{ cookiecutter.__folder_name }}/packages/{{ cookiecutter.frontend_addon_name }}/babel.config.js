bca2fd58b8af667752b3e8ddbae3a6762e4b9929
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-11-19 11:54:04 +0100

    Amend to support 19 and 18

--- templates/add-ons/frontend/{{ cookiecutter.__folder_name }}/packages/{{ cookiecutter.frontend_addon_name }}/babel.config.js
+++ templates/add-ons/frontend/{{ cookiecutter.__folder_name }}/packages/{{ cookiecutter.frontend_addon_name }}/babel.config.js
@@ -1,6 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
+{%- if cookiecutter.volto_version >= '19' %}
   const presets = ['@plone/razzle'];
+{%- else %}
+  const presets = ['razzle'];
+{%- endif %}
   const plugins = [
     [
       'react-intl', // React Intl extractor, required for the whole i18n infrastructure to work
