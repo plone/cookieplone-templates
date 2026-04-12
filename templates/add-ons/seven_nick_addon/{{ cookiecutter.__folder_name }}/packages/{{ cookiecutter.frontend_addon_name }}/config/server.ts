561d34103d7d09242263551157cc56fe82fd2201
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-04-12 12:05:45 +0200

    Right config

--- templates/add-ons/seven_nick_addon/{{ cookiecutter.__folder_name }}/packages/{{ cookiecutter.frontend_addon_name }}/config/server.ts
+++ templates/add-ons/seven_nick_addon/{{ cookiecutter.__folder_name }}/packages/{{ cookiecutter.frontend_addon_name }}/config/server.ts
@@ -2,12 +2,10 @@ import type { ConfigType } from '@plone/registry';
 import { Client } from '@robgietema/nick/src/client';
 
 export default function install(config: ConfigType) {
-  const cli = Client.initialize({ token: undefined });
-
   config.registerUtility({
     name: 'ploneClient',
     type: 'client',
-    method: () => cli,
+    method: () => Client,
   });
 
   return config;
