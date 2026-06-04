cd14c149a77e4a2c1268d53a28a2846d4c4de556
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-06-04 13:52:57 +0200

    REmove

--- seven-add-on/eslint.config.mjs
+++
@@ -1,3 +0,0 @@
-import eslintConfig from './core/eslint.config.mjs';
-
-export default eslintConfig;
--- seven-add-on/registry.config.ts
+++
@@ -1,5 +0,0 @@
-import { addons } from 'seven/registry.config';
-
-addons.push('seven-add-on');
-
-export { addons };
